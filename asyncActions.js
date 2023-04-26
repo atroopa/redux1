2681ae606be6a22c1723dd39845cdc852b4c21c6
atroopa <atroopa@gmail.com>
2023-04-26 19:29:25 +0330

    make actions types

--- asyncActions.js
+++ asyncActions.js
@@ -6,3 +6,9 @@ const initialState = {
     error: ''
 };
 
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_DATA_SUCCESS  = "FETCH_DATA_SUCCESS" ;
+const FETCH_DATA_FAILURE  = "FETCH_DATA_FAILURE" ;
+
+
+
