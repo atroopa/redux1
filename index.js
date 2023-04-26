538ccc0f09f05e2af80f23eb8d1234e3e086dc93
atroopa <atroopa@gmail.com>
2023-04-26 16:56:23 +0330

    not combind yet

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const redux = require('redux');
-const createStore = redux.createStore();
+const redux = require("redux");
+const createStore = redux.createStore;
 
-const BUY_MOB = 'BUY_MOB';
-
+const BUY_MOB    = 'BUY_MOB';
+const BUY_TABLET = 'BUY_TABLET';
 
 const buyMob = () => {
     return {
@@ -11,15 +11,26 @@ const buyMob = () => {
     }
 };
 
+const buyTablet = () => {
+    return {
+        type: BUY_TABLET,
+        info: "second Action"
+    }
+};
+
 const initialState = {
-    numOfMobs: 100
+    numOfMobs: 100,
+    numOfTablet: 50
 };
 
 const reducer = (state=initialState , action) => {
     switch (action.type) {
         case BUY_MOB:
-            return { ... state, numOfMobs: state.numOfMobs-- };
+            return { ... state , numOfMobs: state.numOfMobs-1 };
     
+        case BUY_TABLET:
+            return {... state , numOfTablet: state.numOfTablet -1};
+
         default:
              return state;
     }
@@ -27,4 +38,12 @@ const reducer = (state=initialState , action) => {
 
 
 const store = createStore(reducer);
+console.log("initial State is ", store.getState());
 
+const unsubscribe =  store.subscribe(() => console.log("updated state is ", store.getState()));
+
+
+store.dispatch(buyMob());
+store.dispatch(buyMob());
+store.dispatch(buyTablet());
+store.dispatch(buyTablet());
