02f8e4dee6e9fcc46abac01d80c993a46e62d49c
atroopa <atroopa@gmail.com>
2023-04-26 12:02:05 +0330

    make action creator and reducer

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("First step of Project");
+const BUY_MOB = 'BUY_MOB';
+
+
+const buyMob = () => {
+    return {
+        type: BUY_MOB,
+        info : "first Action"
+    }
+};
+
+const initialState = {
+    numOfMobs: 100
+};
+
+const reducer = (state=initialState , action) => {
+    switch (action.type) {
+        case BUY_MOB:
+            return state.numOfMobs ++;
+    
+        default:
+            state;
+    }
+};
